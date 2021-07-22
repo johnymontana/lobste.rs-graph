f7dbab3b3015097a7fe0cc57bb558d61928feb43
johnymontana <lyonwj@gmail.com>
2021-07-22 16:24:31 -0600

    fix serverless function error

--- next/pages/api/graphql.js
+++ next/pages/api/graphql.js
@@ -2,6 +2,7 @@ import { gql, ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import neo4j from "neo4j-driver";
 import { Neo4jGraphQL } from "@neo4j/graphql";
+import "ts-tiny-invariant"
 
 const typeDefs = gql`
   type Article @exclude(operations: [CREATE, UPDATE, DELETE]) {
